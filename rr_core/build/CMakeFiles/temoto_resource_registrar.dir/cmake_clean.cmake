e951be1503fce0c512882a2d70e0578fb7cfef32
RValner <valnerrobert@gmail.com>
2020-10-20 13:22:42 +0300

    Package restructuring

---
+++ rr_core/build/CMakeFiles/temoto_resource_registrar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temoto_resource_registrar.dir/src/rr_test.cpp.o"
+  "libtemoto_resource_registrar.pdb"
+  "libtemoto_resource_registrar.so.0.0.1"
+  "libtemoto_resource_registrar.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temoto_resource_registrar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
