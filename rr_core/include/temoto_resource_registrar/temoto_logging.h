2441d06b23788e884a900e64a4ebb29636fc3703
Robert <valnerrobert@gmail.com>
2021-05-03 22:08:26 +0300

    Added temoto logging macros

--- rr_core/include/temoto_resource_registrar/temoto_logging.h
+++ rr_core/include/temoto_resource_registrar/temoto_logging.h
@@ -17,7 +17,6 @@
 #ifndef TEMOTO_LOGGING_H
 #define TEMOTO_LOGGING_H
 
-#include <boost/core/demangle.hpp>
 #include <cstdlib>
 #include <map>
 #include <mutex>
@@ -25,15 +24,59 @@
 #include <string>
 #include <thread>
 #include <typeinfo>
+#include <boost/core/demangle.hpp>
+#include <console_bridge/console.h>
 
 #include <iostream> // TODO: remove after debug finished
+#include <sstream>
 
+// Logging prefix macro definitions
 #define GET_NAME_FF TEMOTO_LOG_ATTR.getNsWithSlash() + __func__
 #define GET_NAME TEMOTO_LOG_ATTR.getNsWithSlash() + boost::core::demangle(typeid(*this).name()) + "::" + __func__
 
+// Distribted tracing related macro definitions
 #define PUSH_TRACE_CONTEXT_FF TEMOTO_LOG_ATTR.pushParentTraceContext(GET_NAME)
 #define PUSH_TRACE_CONTEXT TEMOTO_LOG_ATTR.pushParentTraceContext(GET_OBJ_NAME)
 #define PRINT_TRACE_CONTEXT TEMOTO_LOG_ATTR.printContext()
+
+// TeMoto logging related definitions via console bridge
+#define TEMOTO_LOG_(level, fmt, ...) \
+  console_bridge::log(__FILE__, __LINE__, level, std::string("[from "+GET_NAME_FF+"]: "+fmt).c_str(), ##__VA_ARGS__)
+
+// #define TEMOTO_ERROR_(fmt, ...) \
+//   TEMOTO_LOG_(console_bridge::CONSOLE_BRIDGE_LOG_ERROR, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_WARN_(fmt, ...) \
+  TEMOTO_LOG_(console_bridge::CONSOLE_BRIDGE_LOG_WARN, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_INFO_(fmt, ...) \
+  TEMOTO_LOG_(console_bridge::CONSOLE_BRIDGE_LOG_INFO, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_DEBUG_(fmt, ...) \
+  TEMOTO_LOG_(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG, fmt, ##__VA_ARGS__)
+
+// TeMoto logging stream related definitions via console bridge
+#define TEMOTO_LOG_STREAM_(level, fmt, ...) \
+{ \
+  std::stringstream ss; \
+  ss << "[from " << GET_NAME_FF << "]: " << fmt; \
+  console_bridge::log(__FILE__, __LINE__, level, ss.str().c_str(), ##__VA_ARGS__); \
+} \
+
+#define TEMOTO_ERROR_STREAM_(fmt, ...) \
+  TEMOTO_LOG_STREAM_(console_bridge::CONSOLE_BRIDGE_LOG_ERROR, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_WARN_STREAM_(fmt, ...) \
+  TEMOTO_LOG_STREAM_(console_bridge::CONSOLE_BRIDGE_LOG_WARN, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_INFO_STREAM_(fmt, ...) \
+  TEMOTO_LOG_STREAM_(console_bridge::CONSOLE_BRIDGE_LOG_INFO, fmt, ##__VA_ARGS__)
+
+#define TEMOTO_DEBUG_STREAM_(fmt, ...) \
+  TEMOTO_LOG_STREAM_(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG, fmt, ##__VA_ARGS__)
+
+// #define TEMOTO_DEBUG_(fmt, ...)  \
+//   console_bridge::log(__FILE__, __LINE__, console_bridge::CONSOLE_BRIDGE_LOG_DEBUG, "[" + GET_NAME + "]" + fmt, ##__VA_ARGS__)
 
 class TemotoLoggingAttributes
 {
@@ -41,6 +84,7 @@ public:
   typedef std::string TraceContextType;
 
   TemotoLoggingAttributes()
+  : subsystem_name_("")
   {
     if (const char *env_p = std::getenv("TEMOTO_NAMESPACE"))
     {
@@ -68,17 +112,17 @@ public:
     return trace_stacks_.at(std::this_thread::get_id()).top();
   }
 
-  void printContext() const
-  {
-    std::lock_guard<std::recursive_mutex> l(trace_stacks_mtx_);
-    std::stack<TraceContextType> stack_cpy = trace_stacks_.at(std::this_thread::get_id());
+  // void printContext() const
+  // {
+  //   std::lock_guard<std::recursive_mutex> l(trace_stacks_mtx_);
+  //   std::stack<TraceContextType> stack_cpy = trace_stacks_.at(std::this_thread::get_id());
 
-    while (!stack_cpy.empty())
-    {
-      std::cout << stack_cpy.top() << std::endl;
-      stack_cpy.pop();
-    }
-  }
+  //   while (!stack_cpy.empty())
+  //   {
+  //     std::cout << stack_cpy.top() << std::endl;
+  //     stack_cpy.pop();
+  //   }
+  // }
 
   const std::string &getNs() const
   {
@@ -87,18 +131,27 @@ public:
 
   std::string getNsWithSlash() const
   {
+    std::string prefix;
     if (!temoto_namespace_.empty())
     {
-      return temoto_namespace_ + "/";
+      prefix = temoto_namespace_ + "/";
     }
-    else
+    
+    if (!subsystem_name_.empty())
     {
-      return std::string();
+      prefix += subsystem_name_ + "/";
     }
+    return prefix;
+  }
+
+  void setSubsystemName(const std::string& subsystem_name)
+  {
+    subsystem_name_ = subsystem_name;
   }
 
 private:
   std::string temoto_namespace_;
+  std::string subsystem_name_;
   std::map<std::thread::id, std::stack<TraceContextType>> trace_stacks_;
   mutable std::recursive_mutex trace_stacks_mtx_;
 };
